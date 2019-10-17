b217519f0e7459658c51b17562758f99bd4f2c14
Kendall Tames <tamesfern@gmail.com>
2019-10-17 08:55:47 -0600

    Finished genetic feature

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-#!encoding=utf-8
 from probabilistic import Probabilistic
 from image import ImageData
+from genetic import Genetic
 
 if __name__ == "__main__":
     rows = 10
@@ -9,3 +9,6 @@ if __name__ == "__main__":
     images = [ImageData("imgs/guacamaya.jpg"), ImageData("imgs/Beach.jpg"), ImageData("imgs/imagen.jpg")]
     probabilistic = Probabilistic(images, rows, columns)
     sectors = probabilistic.choose_samples()
+
+    genetic = Genetic(sectors, len(images))
+    genetic.run()
