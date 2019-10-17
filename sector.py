33ffcd36eb8d1aa378fb3358c25366b9817f2f94
Kendall Tames <tamesfern@gmail.com>
2019-10-17 09:06:23 -0600

    Fixed indentation

--- sector.py
+++ sector.py
@@ -78,8 +78,9 @@ class Sector:
                 high_bound = (2 ** 16) * (total_sum / image_count) + low_bound
                 color.set_bit_bounds(int(low_bound), int(high_bound))
                 actual_bit_position = int(high_bound)
-                self._colors[last_key].set_max_bound(2 ** 16 - 1)
-            self._create_first_generation()
+
+        self._colors[last_key].set_max_bound(2 ** 16 - 1)
+        self._create_first_generation()
 
     def _create_first_generation(self):
         for index in range(0, 3):
